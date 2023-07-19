34917939bacb29a5045fcda07078b473c5d30cee
RomuloJB <118928688+RomuloJB@users.noreply.github.com>
2023-07-19 15:33:12 -0300

    buscando por numeros no array
    searching for a number in the array

--- busca.py
+++ busca.py
@@ -14,4 +14,4 @@ if(found == -1):
     print(f"{n} Não foi encontrado no vetor") #The number typed wasn't found in the array
 
 else:
-    print(f"O numero {n} existe dentro do vetor a, e está na posição {i}") #The number typed is part of the array and it's located at the {i} position
+    print(f"O numero {n} existe dentro do vetor a, e está na posição {i}") #The number typed is part of the array and it's located at the position {i}
